755d52edbd9fd1a2339ee58869054f0f90a74703
Chris Waterhouse <chris@justanothergeek.xyz>
2023-04-29 17:38:32 +1000

    Removed old work targets.

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -4,14 +4,10 @@ from locust import FastHttpUser, task
 
 # Inherits the FastHttpUser class
 class MyUser(FastHttpUser):
-    connection_timeout = 10.0
+    connection_timeout = 5.0
 
     @task
     def redcatStoresCall(self):
         # client = FastHttpSession class
-        self.client.get("stores", name="Redcat Stores Call")
-
-    @task
-    def redcatMenuCall(self):
-        self.client.get("stores/50/menu", name="Redcat Menu Call")
+        self.client.get("/", name="Root Call")
 
